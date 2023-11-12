fcab424d9a21d7f8ec4b37fd85c7f15baf1e8277
ShyanRoyChoudhury <Shayan.roy31@gmail.com>
2023-11-12 23:13:15 +0530

    header width test in smaller screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC<HeaderProps> = ({className}) => {
     return(
         <div className={twMerge(`flex mb-5 h-12 shadow-lg bg-gradient-to-r font-semibold 
         from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% 
-        border-none w-full`, className)}>
+        border-none w-screen`, className)}>
             
             <h1 className='font-semibold text-3xl pl-2 pt-2'>
                 <Link href={'/'}>FocusTimer</Link>
